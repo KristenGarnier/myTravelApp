4061e2820d6396eceb70e67d7f90ad0c693ad2f7
KristenGarnier <garnier.kristen@icloud.com>
2015-02-12 09:55:56 +0100

    New view + some changes

--- assets/js/routing.js
+++ assets/js/routing.js
@@ -3,6 +3,7 @@ $(document).ready(routing());
 function routing(){
     $('#render').load('views/home.html');
     $(document).on("click","a", routingHandler);
+    $(document).on("click","a.command", command);
 }
 
 function routingHandler(e){
@@ -10,4 +11,9 @@ function routingHandler(e){
     var target = $(this).data("url");
     $("#render").load("views/"+target+'.html');
     $('.button-collapse').sideNav('hide');
+}
+
+function command(e){
+    e.preventDefault;
+    toast('Votre voyage a été ajouté au panier !', 4000);
 }
