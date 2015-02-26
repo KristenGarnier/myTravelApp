d5d31e36f11c6b2a159ab81c47fb39afd3f573e2
KristenGarnier <garnier.kristen@icloud.com>
2015-02-26 16:36:21 +0100

    New version

--- assets/js/routing.js
+++ assets/js/routing.js
@@ -9,22 +9,37 @@ function routing() {
 function routingHandler(e) {
     e.preventDefault;
     var target = $(this).data("url");
-    if (target != "sejours") {
+    if (target != "users" && target != "read-user" ) {
         $("#render").load("views/" + target + '.html');
         $('.button-collapse').sideNav('hide');
     } else {
-        $.getJSON( "http://kristengarnier.com/serveur/item.php", function( data ) {
-            var nom = data.title;
-            var nom = '<h1>' + nom + '</h1>';
-            var content = data.content;
-            var content = '<p>' + content + '</p>';
-            $('#render').html(nom + content);
-        }).fail(function( jqxhr, textStatus, error ) {
-            var err = textStatus + ", " + error;
-            console.log( "Request Failed: " + err );
-        });
-        // $("#render").load("../../serveur/item.php");
+        if(target == 'users'){
+            navigator.geolocation.getCurrentPosition(locInfo, error);
+            $.getJSON("http://local.dev/Api/users/?action=all", function (data) {
+                $('#render').empty();
+                $.each(data, function(id, donnes){
+                    $('#render').append('<div class="row"> <div class="col s12 m6"> <div class="card blue-grey darken-1"> <div class="card-content white-text"> <span class="card-title">' +donnes.nom+ ' ' +donnes.prenom+ '</span> <ul><l1>'+donnes.adresse+'</l1><li>'+donnes.CP+'</li><li>'+donnes.ville+'</li></ul> </div> <div class="card-action"> <a data-id="'+ donnes.id +'" data-url="read-user" class="read-user" href="#">Conslter le profil</a></div> </div> </div> </div>');
+                });
+            });
+
+        }else {
+            e.preventDefault();
+            var id = $(this).data('id');
+            $.getJSON("http://local.dev/Api/users/?action=read&id="+id, function (data) {
+                var donnes = data[0];
+                $('#render').empty();
+                console.log(data);
+                $('#render').append('<div class="row"> <div class="col s12 m6"> <div class="card blue-grey darken-1"> <div class="card-content white-text"> <span class="card-title">' +donnes.nom+ ' ' +donnes.prenom+ '</span> <ul><l1>'+donnes.adresse+'</l1><li>'+donnes.CP+'</li><li>'+donnes.ville+'</li></ul> </div> <div class="card-action"> <a data-id="'+ donnes.id +'" data-url="read-user" class="read-user" href="#">Conslter le profil</a></div> </div> </div> </div>');
+            });
+        }
     }
+}
+
+function locInfo(pos){
+    console.log(pos);
+}
+function error(err) {
+    console.warn('ERROR(' + err.code + '): ' + err.message);
 }
 
 function command(e) {
